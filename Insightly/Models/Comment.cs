c17f7d7ebcf85fba15d6b32ad1272f15fc09ec56
Ahmed Wael <waely@std.mans.edu.eg>
2025-09-02 14:51:35 +0300

    Feat : Added Article and Comment models

---
+++ Insightly/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Insightly.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public string AuthorId { get; set; } =string.Empty;
+    }
+}
