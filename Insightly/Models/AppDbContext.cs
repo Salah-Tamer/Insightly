3c5a216163a31708beb8d419d84681cc9ebc26f5
Salah Tamer <ma9060139@gmail.com>
2025-09-03 11:17:20 +0300

    added models and relations

--- Insightly/Models/AppDbContext.cs
+++ Insightly/Models/AppDbContext.cs
@@ -6,5 +6,71 @@ namespace Insightly.Models
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
         public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<ArticleRead> ArticleReads { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // User
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Name).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.Email).IsRequired();
+                entity.Property(u => u.Password).IsRequired();
+                entity.Property(u => u.Gender).IsRequired().HasMaxLength(10);
+                entity.Property(u => u.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+
+            // Article
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.HasKey(a => a.ArticleId);
+                entity.Property(a => a.Title).IsRequired().HasMaxLength(100);
+                entity.Property(a => a.Content).IsRequired();
+                entity.Property(a => a.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                entity.HasOne(a => a.Author)
+                    .WithMany(u => u.Articles)
+                    .HasForeignKey(a => a.AuthorId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Comment
+            //modelBuilder.Entity<Comment>(entity =>
+            //{
+            //    entity.HasKey(c => c.CommentId);
+            //    entity.Property(c => c.Content).IsRequired();
+            //    entity.Property(c => c.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            //    entity.HasOne(c => c.Author)
+            //        .HasForeignKey(c => c.AuthorId)
+            //        .OnDelete(DeleteBehavior.Cascade);
+
+            //    entity.HasOne(c => c.Article)
+            //        .WithMany(a => a.Comments)
+            //        .HasForeignKey(c => c.ArticleId)
+            //        .OnDelete(DeleteBehavior.Cascade);
+            //});
+
+            // ArticleRead
+            modelBuilder.Entity<ArticleRead>(entity =>
+            {
+                entity.HasKey(ar => ar.Id);
+                entity.Property(ar => ar.ReadAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.HasOne(ar => ar.Article)
+                    .WithMany()
+                    .HasForeignKey(ar => ar.ArticleId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(ar => ar.User)
+                    .WithMany(u => u.ReadArticles)
+                    .HasForeignKey(ar => ar.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
     }
 }
