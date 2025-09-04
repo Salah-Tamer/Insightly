181d5468ebda320b60f281f5e4eeb5868415af3f
Salah Tamer <ma9060139@gmail.com>
2025-09-04 12:57:03 +0300

    feat: enabled identity roles

--- Insightly/Program.cs
+++ Insightly/Program.cs
@@ -8,12 +8,29 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connStr));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>()
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Seed required roles on startup
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    string[] roleNames = ["Admin", "User"];
+    foreach (var roleName in roleNames)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+
+}
 
 if (!app.Environment.IsDevelopment())
 {
