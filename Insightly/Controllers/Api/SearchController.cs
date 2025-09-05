613ff020b7c12ef6ba6fc6d25bce54289f89ca3f
Salah Tamer <121264378+Salah-Tamer@users.noreply.github.com>
2025-09-05 22:14:50 +0300

    feat: added search by title (#13)
    * made SearchaController with view
    
    * solve conflict
    
    * made SearchaController with view
    
    * solve conflict
    
    * feat: search by title
    
    * feat: search by title
    
    ---------
    
    Co-authored-by: ahmed walid <ahmed.walid21323@gmail.com>

---
+++ Insightly/Controllers/Api/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Insightly.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Insightly.Controllers.Api
+{
+    public class SearchController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SearchController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Articles/Search?query=health
+        public async Task<IActionResult> Index(string query)
+        {
+            var articles = await _context.Articles
+                .Where(a => string.IsNullOrEmpty(query) || a.Title.Contains(query))
+                .ToListAsync();
+
+            return View(articles); 
+        }
+    }
+}
