86fb47504a0e22f72baadb77df6b6cee048d2660
Salah Tamer <ma9060139@gmail.com>
2025-09-16 04:41:32 +0300

    refactor: remove search index view

--- Insightly/Controllers/SearchController.cs
+++ Insightly/Controllers/SearchController.cs
@@ -13,33 +13,6 @@ namespace Insightly.Controllers
             _context = context;
         }
 
-        // GET: /Search - Browse all articles or search results
-        public async Task<IActionResult> Index(string query)
-        {
-            var articlesQuery = _context.Articles
-                .Include(a => a.Author)
-                .AsQueryable();
-
-            // If search query is provided, filter articles
-            if (!string.IsNullOrWhiteSpace(query))
-            {
-                var searchTerm = query.Trim().ToLower();
-                articlesQuery = articlesQuery.Where(a => 
-                    a.Title.ToLower().Contains(searchTerm) ||
-                    a.Content.ToLower().Contains(searchTerm) ||
-                    a.Author.Name.ToLower().Contains(searchTerm)
-                );
-            }
-
-            var articles = await articlesQuery
-                .OrderByDescending(a => a.CreatedAt)
-                .ToListAsync();
-
-            ViewBag.SearchQuery = query;
-            ViewBag.HasSearchQuery = !string.IsNullOrWhiteSpace(query);
-
-            return View(articles); 
-        }
 
         [HttpGet]
         public async Task<IActionResult> SearchAjax(string query)
