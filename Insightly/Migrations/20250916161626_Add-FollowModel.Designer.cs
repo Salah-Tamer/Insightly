a47814a0dd957f7b52f3baad066e3c26c3d875df
ahmedelgyar1 <63425509+ahmedelgyar1@users.noreply.github.com>
2025-09-17 07:18:55 +0300

    Add Follow putton (#25)
    * Add Follow putton
    
    * Add Follow putton to my profile

--- Insightly/Migrations/20250916161626_Add-FollowModel.Designer.cs
+++ Insightly/Migrations/20250916161626_Add-FollowModel.Designer.cs
@@ -4,6 +4,7 @@ using Insightly.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Insightly.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250916161626_Add-FollowModel")]
+    partial class AddFollowModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
